07a62af5fa359c5954eed078c720ec6f3c98c96b
mlinarik <40384547+mlinarik@users.noreply.github.com>
2021-10-31 15:29:08 -0400

    Create docker_cleanup.sh

---
+++ docker_cleanup.sh
@@ -0,0 +1,30 @@
+mlinarik@dockerdev:~$ cat docker_cleanup 
+#!/bin/bash
+
+echo "1) Delete Volumes"
+echo "2) Delete Networks"
+echo "3) Delete Images"
+echo "4) Delete Containers"
+
+echo "Select Option: "
+read option
+
+if [[ $option -eq 1 ]];
+then
+    echo "Volume Delete"
+    docker volume ls -qf dangling=true | xargs -r docker volume rm
+elif [[ $option -eq 2 ]];
+then
+    echo "Network Delete"
+    docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+elif [[ $option -eq 3 ]];
+then
+    echo "Image Delete"
+    docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
+elif [[ $option -eq 4 ]];
+then
+    echo "Containers Delete"
+    docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+else
+   echo "Invalid Selection"
+fi
